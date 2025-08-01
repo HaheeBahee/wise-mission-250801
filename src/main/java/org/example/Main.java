bf02044154e51cf5da63e09105a79700212a25f5
YOONHEE <parkadg@naver.com>
2025-08-01 15:35:25 +0900

    기존 소스코드 복사 및 초기 설정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine(); //입력값 받기
+
+            if (command.equals("등록")) {
+                System.out.print("명언: ");
+                sc.nextLine();
+                System.out.print("작가: ");
+                sc.nextLine();
+            } else if (command.equals("종료")) {
+                break;
+            }
+
+        }
+
+
+    }
 }
 
