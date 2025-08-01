db31df90a682cbf4330749166034c58cfd7b4918
YOONHEE <parkadg@naver.com>
2025-08-01 15:48:01 +0900

    4단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.println("== 명언 앱 ==");
+        int number = 1;
+
         while (true) {
             System.out.print("명령) ");
             String command = sc.nextLine(); //입력값 받기
@@ -17,7 +18,8 @@ public class Main {
                 sc.nextLine();
                 System.out.print("작가: ");
                 sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(number));
+                number++;
             } else if (command.equals("종료")) {
                 break;
             }
